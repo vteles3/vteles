f3db50c42177a3cff4db45649588bd09faf0ab36
vteles3 <duongpm13vnp@gmail.com>
2022-10-10 10:43:08 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);
+    $("#test-content").text(xmlHttp.responseText);
+    return xmlHttp.responseText;
+}
+
+$(document).ready(function() {
+  httpGet("http://34.125.76.7:3000/ping");
+});
