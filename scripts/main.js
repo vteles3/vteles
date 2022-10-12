d4b4c7b484790679b3e8eedb65e24223da017c1d
vteles3 <duongpm13vnp@gmail.com>
2022-10-12 15:59:04 +0700

    back to loopback

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const userAction = async () => {
+  const response = await fetch('https://34.125.76.7:3000/ping');
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log(myJson);
+}
+
+userAction();
